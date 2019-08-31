989d31eaab3bcf8b56d51d3886f64f5688a216f5
kitazaki <kitazaki@gmail.com>
2019-08-31 14:47:01 +0900

    Add files via upload

--- message.sh
+++ message.sh
@@ -3,10 +3,10 @@ if [ $# -ne 1 ]; then
   echo "usage: message.sh message" 1>&2
   exit 1
 fi
-./hub-ctrl -b 20 -d 6 -P 2 -p 1
+sudo ./hub-ctrl -b 20 -d 6 -P 2 -p 1
 sleep 2
 sudo python3 ./message.py $1
 sudo python3 ./led-badge-11x44.py message.png
 sleep 1
-./hub-ctrl -b 20 -d 6 -P 2 -p 0
+sudo ./hub-ctrl -b 20 -d 6 -P 2 -p 0
 
